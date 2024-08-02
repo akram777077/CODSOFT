d8299d61a14508acde289aced1e3f61cc1fc15c3
akram <akram7.dris7@gmail.com>
2024-08-03 00:47:08 +0100

    make class to save the result of game and store it into file to get high score of the game

---
+++ clsResult.h
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<fstream>
+class clsResult
+{
+    private:
+        int targetNumber;
+        int numberRound;
+        int start;
+        int end;
+        std::string toLine() const {
+            std::ostringstream oss;
+            oss << targetNumber << " " << numberRound << " " << start << " "<<end;
+            return oss.str();
+        }
+    public:
+        void print()
+        {
+            std::cout<<"target Number: "<<targetNumber;
+            std::cout<<"number rounds: "<<numberRound;
+            std::cout<<"start: "<<start;
+            std::cout<<"end: "<<end;
+        }
+        static std::string fileName;
+        clsResult(int targetNumber,int numberRound,int start,int end)
+        {
+            this->targetNumber=targetNumber;
+            this->numberRound=numberRound;
+            this->start=start;
+            this->end=end;
+        }
+        bool save()
+        {
+            std::fstream file;
+            file.open(fileName,std::ios::out);
+            if(!file)
+                return 0;
+            file<<toLine();
+            file.close();
+            return 1;
+        }
+        static clsResult* load()
+        {
+            std::string line="";
+            std::fstream file;
+            file.open(fileName);
+            if(!file)
+                return nullptr;
+            std::getline(file,line);
+            file.close();
+            int arr[4];
+            std::istringstream iss(line);
+            iss >> arr[0] >> arr[1] >> arr[2] >> arr[3];
+            return new clsResult(arr[0],arr[1],arr[2],arr[3]);
+        }
+};
