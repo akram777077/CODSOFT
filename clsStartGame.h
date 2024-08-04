d15f88b6e899ecc9fa38a40d494b2a51070f8d37
akram <akram7.dris7@gmail.com>
2024-08-04 10:20:20 +0100

    fix the bug when play your first game and the file is don't exist

--- clsStartGame.h
+++ clsStartGame.h
@@ -3,7 +3,7 @@
 #include"clsResult.h"
 #include"clsGame.h"
 enum enChois{ex=0,startGame,showHighScore};
-std::string clsResult::fileName="test.txt";
+std::string clsResult::fileName="test1.txt";
 class clsStartGame
 {
     private:
@@ -29,6 +29,18 @@ class clsStartGame
             }while(!result);
             return input;
         }
+        static void showScore(clsResult result)
+        {
+            clsScreens::clearScreen();
+            clsResult* highScore = clsResult::load();
+            bool isHighScore =!highScore || (result.numberRound < highScore->numberRound);
+            clsScreens::showHighScore(0,&result);
+            if(isHighScore && result.save())
+                std::cout<<"[System] -> New High Score is saved...\n";
+            
+            clsScreens::pressEnter();
+            
+        }
         static void startGame()
         {
             clsGame game;
@@ -41,7 +53,14 @@ class clsStartGame
                 
             }while(remark!=enRemark::found);
             std::cout<<"[Game] -> You found the number "<<game.getNumber()<<" after "<<count<<" time(s)\n";
+            clsResult result(game.getNumber(),count,0,100);
             clsScreens::pressEnter();
+            showScore(result);
+        }
+        static void showHighScoreScreen()
+        {
+           clsScreens::showHighScore(1,clsResult::load());
+           clsScreens::pressEnter();
         }
         static void doWithChois(enChois chois)
         {
@@ -49,28 +68,33 @@ class clsStartGame
             switch(chois)
             {
                 case enChois::ex:
+                    exit(0);
                     break;
                 case enChois::startGame:
                     startGame();
                     break;
                 case enChois::showHighScore:
+                    showHighScoreScreen();
                     break;
             }
         }
         static void NewGame()
         {
-            int chois=0;
-            bool result = 1;
-            clsScreens::clearScreen();
-            clsScreens::showMain();
             do
             {
-                std::cout<<"[Chois] -> ";
-                result = clsValidation::IsBetween(chois,0,2);
-                if(!result)
-                    std::cout<<"[System] -> enter valid number please [0,2]\n";
-            }while (!result);
-            doWithChois((enChois)chois);
+                int chois=0;
+                bool result = 1;
+                clsScreens::clearScreen();
+                clsScreens::showMain();
+                do
+                {
+                    std::cout<<"[Chois] -> ";
+                    result = clsValidation::IsBetween(chois,0,2);
+                    if(!result)
+                        std::cout<<"[System] -> enter valid number please [0,2]\n";
+                }while (!result);
+                doWithChois((enChois)chois);
+            }while(1);
         }
     public:
         static void start()
