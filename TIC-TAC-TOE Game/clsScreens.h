a0ae3eee2f264774e24913e4750f181f0b56f9fb
akram <akram7.dris7@gmail.com>
2024-08-11 10:57:59 +0100

    add select mark for mod of play screen

---
+++ TIC-TAC-TOE Game/clsScreens.h
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<limits>
+class clsScreens
+{
+    public:
+    static void pressEnter()
+    {
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        std::cout<<"Press Any Key to continue...";
+        getchar();
+    }
+    static void selectMarks()
+    {
+        std::cout<<"              Select Mod\n";
+        std::cout<<"----------------------------------------\n";
+        std::cout<<"[1] player 1 with 'X' player 2 with 'O'.\n";
+        std::cout<<"[2] player 1 with 'O' player 2 with 'x'.\n";
+        std::cout<<"----------------------------------------\n";
+    }
+};
